9f008f39a440f8bd6b6062ea8b84e279724e07b1
vikas <vikasyaduvanshi161@gmail.com>
2022-05-08 11:41:07 +0530

    first commit3

--- src/App.js
+++ src/App.js
@@ -2,31 +2,91 @@ import React, { useState } from "react";
 import "./App.css";
 import List from "./Component/List";
 function App() {
-  // let [Data, setData] = useState(false);
-  let [value1 , setValue] = useState(null);
+  const [value1, setValue] = useState("");
+  const [todoValue, setTodoValue] = useState([
+    {
+      title: "arun",
+      id: 1,
+      createdAt: new Date().toLocaleString(),
+      checked: false,
+    },
+    {
+      title: "vikas",
+      id: 2,
+      createdAt: new Date().toLocaleString(),
+      checked: false,
+    },
+  ]);
+
+  const Change = (e) => {
+    setValue(e.target.value);
+  };
+
+  const HandelClick = () => {
+    let obj = {
+      title: value1,
+      id: todoValue.length + 1,
+      createdAt: new Date().toLocaleString(),
+      checked: false,
+    };
+    setTodoValue([...todoValue, obj]);
+    setValue("");
+  };
+
+  const deleteItem = (id) => {
+    const filteredArray = todoValue.filter((item) => {
+      return item.id != id;
+    });
+
+    setTodoValue(filteredArray);
+  };
+
+  let HandelCheck = (id) => {
+    const mappedArray = todoValue.map((item) => {
+      if (item.id == id) {
+        item.checked = !item.checked;
+        return item;
+      } else {
+        return item;
+      }
+    });
+    console.log(mappedArray, "===========><==========");
+    setTodoValue(mappedArray);
+  };
+  const DeleteAll = () => {
+    const DeleteAllarray = todoValue.filter((item) => {
+      return item.checked == !true;
+    });
+    setTodoValue(DeleteAllarray);
+  };
 
   return (
     <div className="name">
-      <h2>{value1}</h2>
-  
-   
+      <div>
+        <button onClick={DeleteAll}>DELETE ALL</button>
+      </div>
       <div className="main">
         <div className="App">
           <input
+            value={value1}
+            onChange={Change}
             placeholder="Write a task"
             className="input"
             type="text"
-            onChange={(e) => setValue(e.target.value)}
-            
           />
-          <button className="button">
+          <button className="button" onClick={HandelClick}>
             create
           </button>
         </div>
         <div>
-          <List />
+          {todoValue.map((item) => (
+            <List
+              name={item}
+              deleteItem={deleteItem}
+              HandelCheck={HandelCheck}
+            />
+          ))}
         </div>
-        <header className="App-header"></header>
       </div>
     </div>
   );
--- src/Component/List.js
+++ src/Component/List.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-const List = () => {
+const List = (prop) => {
+  const { name, deleteItem, HandelCheck } = prop;
   return (
-    <div className="List">
-      <input className="checkbox" type="checkbox"></input>
-
-      <h3 className="text"></h3>
-      <button className="delete">edit</button>
-      <button className="edit">delete</button>
+    <div style={{ backgroundColor: "#4e5056", padding: 15, marginTop: 20 }}>
+      <div className="List">
+        <div style={{ display: "flex", alignItems: "center", width: "80%" }}>
+          <button
+            className={name.checked ? "checkboxTrue" : "checkbox"}
+            onClick={() => HandelCheck(name.id)}
+          ></button>
+          <h3 className="text">{name.title}</h3>
+        </div>
+        <div style={{ width: "20%", justifyContent: "space-between" }}>
+          <button className="delete" onClick={() => deleteItem(name.id)}>
+            Delete
+          </button>
+          <button className="edit">Edit</button>
+        </div>
+      </div>
+      <p style={{ color: "#fff", margin: "auto" }}>{name.createdAt}</p>
     </div>
   );
 };
